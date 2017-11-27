fb62c47c92b2d45aab47cf376eddf2735904ddf1
Hun Jae Lee <hunjael@gmail.com>
2017-11-27 15:58:39 -0500

    :sparkles: almost done

--- lib/markov.rb
+++ lib/markov.rb
@@ -4,7 +4,7 @@
 require 'pp'
 
 class Markov
-  attr_accessor :max_chain_length, :tokens, :raw_mapping, :mapping
+  attr_accessor :max_chain_length, :tokens, :raw_mapping, :mapping, :starts
 
   def initialize(text, opts = {})
     raise ArgumentError.new("invalid chain length") if opts[:max_chain_length] < 1
@@ -27,6 +27,7 @@ class Markov
     @max_chain_length = opts[:max_chain_length] || 3
     build_mapping_with_markov_length @tokens, @max_chain_length
     normalize_mapping
+    p generate_sentence @max_chain_length
   end
 
   # Returns the contents of the file, split into a list of words and punctuations.
@@ -50,26 +51,25 @@ class Markov
         if @raw_mapping[history].has_key? word
           @raw_mapping[history][word] += 1.0
         else
-          @raw_mapping[history][word] = 1.0
+          @raw_mapping[history][word.dup] = 1.0
         end
       else
         @raw_mapping[history.dup] = {}
-        @raw_mapping[history][word] = 1.0
+        @raw_mapping[history][word.dup] = 1.0
       end
       history.shift
     end
-    @raw_mapping
   end
 
   # word_list - [[String]] list of sentences, split by each word.
   def build_mapping_with_markov_length word_list, markov_length
     word_list.each do |sentence|
       @starts << sentence.first
-      sentence.each_with_index do |word, idx|
+      (1..sentence.size-1).each do |idx|
         if idx <= markov_length
           history = sentence[0..idx]
         else
-          history = sentence[idx-markov_length+1..idx+1]
+          history = sentence[idx - markov_length+1..idx]
         end
         follow = sentence[idx+1]
         add_to_raw_mapping history, follow
@@ -87,6 +87,48 @@ class Markov
       end
     end
     @mapping
+  end
+
+  def next_word_from prev_list
+    sum = 0.0
+    idx = Random.rand
+    return_str = ""
+
+    # Shorten prevList until it's in mapping
+    until @mapping.include? prev_list
+      prev_list.shift
+      if prev_list.empty?
+        return nil
+      end
+    end
+
+    # Get a random word from the mapping, given prevList
+    @mapping[prev_list].each do |next_word, frequency|
+      sum += frequency
+      if sum >= idx
+        return_str = next_word
+      end
+    end
+
+    return_str
+  end
+
+  def generate_sentence markov_length
+    current = @starts.sample
+    sentence = ""
+    prev_list = [current]
+    # keep adding words until we hit end of sentence.
+    until current.nil?
+      sentence << current
+      sentence << " "
+      current = next_word_from prev_list
+      prev_list << current
+      # if the prev_list has gotten too long, trim it.
+      if prev_list.size > markov_length
+        prev_list.shift
+      end
+    end
+    sentence
   end
 
   # TODO: We want to be able to compare words independent of their capitalization.
